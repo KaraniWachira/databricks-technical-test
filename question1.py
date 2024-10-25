732d34f44344e3aa960190fd4f8ea84e0e94980f
Karani Wachira <wachirakeith@gmail.com>
2024-10-25 15:49:19 +0300

    feat: add question1 comment

---
+++ question1.py
@@ -0,0 +1,6 @@
+# Write a query that will display the results below (Note: some columns might be renamed
+# but use the column names above). It should only show 2020 data and order by driver
+# points.
+
+
+
