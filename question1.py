cdef8334b1de5455ccb5ef5b34a6e452168fbe59
Karani Wachira <wachirakeith@gmail.com>
2024-10-29 02:36:54 +0300

    feat:added solution to question1

--- question1.py
+++ question1.py
@@ -3,4 +3,11 @@
 # points.
 
 
+# Filter the data to only show rows where the year is 2020. I took the dataframe name as race_results
+race_results_2020 = race_results[race_results['year'] == 2020]
 
+# Sort the results by driver points in descending order from the dataframe loaded
+race_results_2020 = race_results_2020.sort_values(by='points', ascending=False)
+
+# Display the results
+print(race_results_2020)
